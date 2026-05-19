cabc1d56cb27ab886e82552e8887cc24e01c4da6
kukushca <tinikitos@gmail.com>
2026-05-19 20:28:01 +0700

    Fix video source URL double binding

--- frontend/src/VideoList.js
+++ frontend/src/VideoList.js
@@ -28,7 +28,7 @@ function VideoList() {
                             <h3 style={{ color: '#ffffff' }}>{video.title}</h3>
                             <p style={{ color: '#e0e0e0' }}>Автор: {video.author}</p>
                             <video controls style={{ width: '100%', borderRadius: '6px', backgroundColor: '#000' }}>
-                                <source src={`http://127.0.0.1:5000${video.stream_url}`} type="video/mp4" />
+                                <source src={video.stream_url} type="video/mp4" />
                             </video>
                         </div>
                     ))}
