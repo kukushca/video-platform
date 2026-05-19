69f868d0e2a5d1e70d49d188dde2372a2ff4ad1c
kukushca <tinikitos@gmail.com>
2026-05-19 19:17:27 +0700

    Connect frontend to production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api'
+    baseURL: 'https://video-platform-kqvw.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
